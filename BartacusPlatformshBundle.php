aca25c866315e1258d51a3a201a37590722055cb
Patrik Karisch <patrik@karisch.guru>
2017-09-04 13:52:47 +0200

    Create simply bundle with nothing in it yet

---
+++ BartacusPlatformshBundle.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Bartacus Platform.sh bundle.
+ *
+ * (c) Patrik Karisch <patrik@karisch.guru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bartacus\Bundle\PlatformshBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BartacusPlatformshBundle extends Bundle
+{
+}
---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/) 
+and this project adheres to [Semantic Versioning](http://semver.org/).
+
+## [Unreleased]
+### Added
+- Create simply bundle with nothing in it yet
+
+[Unreleased]: https://github.com/Bartacus/BartacusPlatformshBundle/compare/d84fd9f...HEAD
