19c8dbb5e213f0ef11bbc947ddf07188d88c051c
Stefan Herndler <s.herndler@pixelart.at>
2023-02-27 14:15:23 +0100

    Drop TYPO3 v10 support

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '4.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-11.9.99',
+            'typo3' => '11.5.0-11.9.99',
         ],
     ],
 ];
